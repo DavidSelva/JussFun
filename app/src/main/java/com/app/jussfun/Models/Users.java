681ad7120621f460f4eee611095dd9a029a5f2a3
Selva <selvamurugan009@gmail.com>
2021-11-14 00:21:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/app/jussfun/Models/Users.java
@@ -0,0 +1,26 @@
+package com.app.jussfun.Models;
+
+import java.util.ArrayList;
+
+public class Users {
+
+
+    private ArrayList<Images> images;
+
+    public Users() {
+    }
+
+    public Users(ArrayList<Images> images) {
+        this.images = images;
+    }
+
+    public ArrayList<Images> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<Images> images) {
+        this.images = images;
+    }
+
+
+}
